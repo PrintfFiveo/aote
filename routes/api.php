91f79934498b0d2cb9211ef983a96e04f3c74bf5
printffiveo <vitor.tavares.leite@gmail.com>
2024-09-05 18:51:24 -0300

    test

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,15 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\presenca;
+use App\Http\Controllers\TurmaController;
+
+
+
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +25,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('/presentes', presenca::class);
+Route::apiResource('/users', UserController::class );
+Route::apiResource('/turmas', TurmaController::class );
+Route::patch('/user/{id}', [UserController::class, 'patch' ]);
+Route::put('/users/{id}', [UserController::class, 'update' ]);  
+Route::post('/user', [UserController::class, 'store']);
+Route::get('/turmas/search/{id}', [TurmaController::class, 'searchByTurma' ]);
+Route::get('/turmas/by-aluno/{id}', [TurmaController::class, 'searchByAluno']);
